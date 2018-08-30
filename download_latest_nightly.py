3c96de8916974458eb20a97da98d4d90428ddf4e
MarcoFalke <falke.marco@gmail.com>
2018-08-30 12:03:43 -0400

    Add valgrind tests

--- download_latest_nightly.py
+++ download_latest_nightly.py
@@ -6,7 +6,7 @@ import shutil
 
 LATEST_URL = 'https://bitcoin.jonasschnelli.ch/build/nightly/latest'
 BUILD_URL = 'https://bitcointools.jonasschnelli.ch/data/builds/{}/{}'
-ARCHIVE_RE = 'bitcoin-0\.[0-9]+\.99-osx64\.tar\.gz'
+ARCHIVE_RE = 'bitcoin-0\.[0-9]+\.99-x86_64-linux-gnu(-debug)?\.tar\.gz'
 ARCHIVE_EXT = 'tar.gz'
 
 
@@ -28,22 +28,32 @@ def main():
     print('build id: {}'.format(build_id))
 
     for line in get_lines(BUILD_URL.format(build_id, '')):
-        if '-osx64.tar.gz' in line:
+        if '-x86_64-linux-gnu.tar.gz' in line:
             archive_gitian_name = re.sub('^.*({}).*$'.format(ARCHIVE_RE),
                                          '\g<1>', line.strip())
+        if '-x86_64-linux-gnu-debug.tar.gz' in line:
+            archive_gitian_name_dbg = re.sub('^.*({}).*$'.format(ARCHIVE_RE),
+                                         '\g<1>', line.strip())
     print('filename: {}'.format(archive_gitian_name))
+    print('filename: {}'.format(archive_gitian_name_dbg))
     version = int(
         re.sub('bitcoin-0.(\d+).99-.*', '\g<1>', archive_gitian_name))
     print('version: {}'.format(version))
 
     archive_name = 'bitcoin-core-nightly.{}'.format(ARCHIVE_EXT)
+    archive_name_dbg = 'bitcoin-core-nightly-dbg.{}'.format(ARCHIVE_EXT)
     with open(archive_name, 'wb') as archive:
         archive.write(
             urllib.request.urlopen(
                 BUILD_URL.format(build_id, archive_gitian_name)).read())
+    with open(archive_name_dbg, 'wb') as archive:
+        archive.write(
+            urllib.request.urlopen(
+                BUILD_URL.format(build_id, archive_gitian_name_dbg)).read())
 
     build_dir = os.path.join(root_folder, 'build_dir')
     shutil.unpack_archive(archive_name, build_dir)
+    shutil.unpack_archive(archive_name_dbg, build_dir)
     build_dir = os.path.join(build_dir, 'bitcoin-0.{}.99'.format(version), '')
 
     build_dir_src = os.path.join(build_dir, 'src')
